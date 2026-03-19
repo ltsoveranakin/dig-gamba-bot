aafec933f63619cae7b0a144148f79fd4555ff8f
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-03-19 18:34:38 -0400

    Move inventory command to its own file, add help info for iteminfo command

--- src/commands/inventory/item_info.rs
+++ src/commands/inventory/item_info.rs
@@ -1,7 +1,10 @@
 use crate::commands::{default_embed, default_reply, CommandContext, DigCommandError};
-use crate::db::schema::item::Item;
+use crate::db::schema::item::{Item, ItemValue};
 use serenity::all::{CreateAttachment, CreateEmbedAuthor};
 
+/// Gives information on a specified item
+///
+/// For example, to get information on the ruby item you can use /iteminfo Ruby
 #[poise::command(slash_command, category = "inventory", rename = "iteminfo")]
 pub(super) async fn item_info(
     ctx: CommandContext<'_>,
@@ -18,11 +21,15 @@ pub(super) async fn item_info(
             .embed(
                 default_embed()
                     .author(
-                        CreateEmbedAuthor::new(item_name)
+                        CreateEmbedAuthor::new(item_name.clone())
                             .icon_url(format!("attachment://{}", item_file_name)),
                     )
                     .title(item_name)
-                    .description(item.get_description()),
+                    .description(format!(
+                        "{}\nBase item value: {}",
+                        item.get_description(),
+                        item.get_item_value()
+                    )),
             )
             .attachment(attachment),
     )
